d2848e141f6a3c2cef2c3a9c97202af445ba1eb5
Caster <54877116+PlayableCaster@users.noreply.github.com>
2020-02-18 21:12:06 -0600

    New Suu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 
 
-        $(function () {
-            $('.go-down').click(function () {
-                $('html,body').animate({scrollTop: $('#after-hero').offset().top}, 500);
+        $(function() {
+            /* hamburger menu */
+            $('.close-btn, .open-btn').click(function() {
+                $('body').toggleClass('is-not-active is-active');
             });
-        });
+        
+            /*
+                animation widget from here: https://github.com/silexlabs/Silex/issues/443
+            */
+        
+            window.sr = ScrollReveal({
+                distance: '100px'
+            });
+            sr.reveal('.from-left', { origin:  'left'});
+            sr.reveal('.from-right', { origin:  'right'});
+            sr.reveal('.from-top', { origin:  'top'});
+            sr.reveal('.from-bottom', { origin:  'bottom'});
+        
+        })
     
